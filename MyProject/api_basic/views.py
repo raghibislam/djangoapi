a82b2e0ce1082d77ab2bb6e9b52e26c704a1a7fe
raghibislam <islamraghib3@gmail.com>
2021-05-10 03:11:12 +0545

    genericviwset

--- MyProject/api_basic/views.py
+++ MyProject/api_basic/views.py
@@ -17,25 +17,10 @@ from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
-class ArticleViewSet(viewsets.ViewSet):
-    def list(self,request):
-        articles = Article.objects.all()
-        serializer = ArticleSerializer(articles , many=True)
-        return Response(serializer.data)
-
-    def create(self,request):
-        serializer = ArticleSerializer(data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data,status=status.HTTP_201_CREATED)
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-
-    def retrieve(self,request,pk=None):
-         queryset = Article.objects.all()
-         article = get_object_or_404(queryset,pk=pk)
-         serializer = ArticleSerializer(article)
-         return Response(serializer.data)
+class ArticleViewSet(viewsets.GenericViewSet,mixins.ListModelMixin,mixins.CreateModelMixin,mixins.UpdateModelMixin,mixins.RetrieveModelMixin,mixins.DestroyModelMixin):
+    serializer_class =  ArticleSerializer
+    queryset = Article.objects.all()
+    
     
     def update(self,request,pk=None):
         article = Article.objects.get(pk=pk)
