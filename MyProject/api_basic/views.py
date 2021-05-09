f0cede8de22ba8abf0cc06cdf95404644a63e418
raghibislam <islamraghib3@gmail.com>
2021-05-10 02:37:57 +0545

    Basic auth

--- MyProject/api_basic/views.py
+++ MyProject/api_basic/views.py
@@ -10,6 +10,8 @@ from rest_framework import status
 from rest_framework.views  import APIView
 from rest_framework import generics
 from rest_framework import mixins
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
@@ -17,6 +19,8 @@ class GenericAPIView(generics.GenericAPIView,mixins.ListModelMixin,mixins.Create
     serializer_class =  ArticleSerializer
     queryset = Article.objects.all()
     lookup_field = 'id'
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
     def get(self,request,id=None):
         if id:
