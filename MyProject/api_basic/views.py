583b2bb255130ff62037974040c444643e7f6e2a
raghibislam <islamraghib3@gmail.com>
2021-05-10 01:55:12 +0545

    Class Based API view

--- MyProject/api_basic/views.py
+++ MyProject/api_basic/views.py
@@ -7,9 +7,30 @@ from .serializers import ArticleSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-
+from rest_framework.views  import APIView
+# from rest_framework import generics
+# from rest_framework import mixins
 
 # Create your views here.
+
+
+
+
+class ArticleAPIView(APIView):
+    def get(self,request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles , many=True)
+        return Response(serializer.data)
+
+    def post(self,request):
+        serializer = ArticleSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+         
 
 @api_view(['GET','POST'])
 def article_list(request):
