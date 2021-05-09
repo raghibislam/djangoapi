198e8d40c589464fff3dacef038878ec1b855b00
raghibislam <islamraghib3@gmail.com>
2021-05-10 02:43:18 +0545

    TokenAuth

--- MyProject/MyProject/settings.py
+++ MyProject/MyProject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api_basic',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
--- MyProject/api_basic/views.py
+++ MyProject/api_basic/views.py
@@ -10,7 +10,7 @@ from rest_framework import status
 from rest_framework.views  import APIView
 from rest_framework import generics
 from rest_framework import mixins
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.authentication import SessionAuthentication,TokenAuthentication, BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
@@ -19,7 +19,8 @@ class GenericAPIView(generics.GenericAPIView,mixins.ListModelMixin,mixins.Create
     serializer_class =  ArticleSerializer
     queryset = Article.objects.all()
     lookup_field = 'id'
-    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    # authentication_classes = [SessionAuthentication, BasicAuthentication]
+    authentication_classes=[TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get(self,request,id=None):
